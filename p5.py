85883884edd9b0f6460c07cc083d16faf36f049d
danny-423 <122214622+danny-423@users.noreply.github.com>
2023-01-08 21:18:00 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -14,23 +14,14 @@ def findMinRoute(tsp):
 	i = 0
 	min = INT_MAX
 	visitedRouteList = DefaultDict(int)
-
-	# Starting from the 0th indexed
-	# city i.e., the first city
+	
 	visitedRouteList[0] = 1
 	route = [0] * len(tsp)
 
-	# Traverse the adjacency
-	# matrix tsp[][]
 	while i < len(tsp) and j < len(tsp[i]):
-
-		# Corner of the Matrix
 		if counter >= len(tsp[i]) - 1:
 			break
 
-		# If this path is unvisited then
-		# and if the cost is less then
-		# update the cost
 		if j != i and (visitedRouteList[j] == 0):
 			if tsp[i][j] < min:
 				min = tsp[i][j]
@@ -38,8 +29,6 @@ def findMinRoute(tsp):
 
 		j += 1
 
-		# Check all paths from the
-		# ith indexed city
 		if j == len(tsp[i]):
 			sum += min
 			min = INT_MAX
@@ -48,8 +37,6 @@ def findMinRoute(tsp):
 			i = route[counter] - 1
 			counter += 1
 
-	# Update the ending city in array
-	# from city which was last visited
 	i = route[counter - 1] - 1
 
 	for j in range(len(tsp)):
@@ -60,16 +47,9 @@ def findMinRoute(tsp):
 
 	sum += min
 
-	# Started from the node where
-	# we finished as well.
 	print("Minimum Cost is :", sum)
 
-
-# Driver Code
 if __name__ == "__main__":
 
-	# Input Matrix
 	tsp = [[-1, 7,11,12, 15], [7, -1, 20,10,12], [11,20,-1,13,17], [12,10,13,-1,5],[15,12,17,5,-1]]
-
-	# Function Call
 	findMinRoute(tsp)
